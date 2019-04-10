39fc806cfb97c68925611c65c496a194d536b139
Kabiru Abdulhamid <abdulhamidkabiru@gmail.com>
2019-04-10 03:18:00 +0100

    fix whitenoisej

--- pmp/wsgi.py
+++ pmp/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
+
+
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -16,3 +19,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pmp.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
+
+
+
+"""
+import os
+from whitenoise import WhiteNoise
+from pmp import users
+
+application = MyWSGIApp()
+application = WhiteNoise(application, root='/path/users/static/')
+application.add_files('/path/users/static/files', prefix='more-files/')
+"""
