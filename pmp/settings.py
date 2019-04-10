06aaea5abfad81eb9b8f697a6470f8edfb9c763c
Kabiru Abdulhamid <abdulhamidkabiru@gmail.com>
2019-04-10 23:13:12 +0100

    static roogff

--- pmp/settings.py
+++ pmp/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') 
+STATIC_ROOT = 'users/static'
 STATIC_URL = '/static/'  
 STATICFILES_DIRS = (
      os.path.join(PROJECT_ROOT, 'static'),
