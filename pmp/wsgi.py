df9a61bccae561516aab43d44ac1200429846694
Sherif Siyanbola <sherifsiyanbola@gmail.com>
2019-10-31 14:39:13 +0100

    fix whitenioise error

--- pmp/wsgi.py
+++ pmp/wsgi.py
@@ -14,9 +14,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-#from whitenoise.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pmp.settings')
 
 application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
