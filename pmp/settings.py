7fadc2ffce7b8b424d7da81d745d6aebce8f2306
Kabiru Abdulhamid <abdulhamidkabiru@gmail.com>
2019-10-31 01:19:11 +0100

    changed the pwa start pont to deploy

--- pmp/settings.py
+++ pmp/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-#from decouple import config
-#import dj_database_url
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = '97=ti2+&!62ss9(*zr%5#h+0a7)@vmueug@zcs!2kp^o2+@6p#'
-#SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = '97=ti2+&!62ss9(*zr%5#h+0a7)@vmueug@zcs!2kp^o2+@6p#'
+SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -86,15 +86,15 @@ WSGI_APPLICATION = 'pmp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = { 'default': dj_database_url.config( default=config('DATABASE_URL') ) }
+DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+# 'default': {
+#     'ENGINE': 'django.db.backends.sqlite3',
+#      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#   }
+# }
 
 
 # Password validation
