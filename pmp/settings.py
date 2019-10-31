8c29e19dda15bb68fd8ffe135296008970ccff93
Sherif Siyanbola <sherifsiyanbola@gmail.com>
2019-10-31 14:36:21 +0100

    added filter

--- pmp/settings.py
+++ pmp/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 AUTH_USER_MODEL = 'users.User'
