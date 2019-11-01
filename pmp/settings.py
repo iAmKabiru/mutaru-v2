3930f2a4a94fab427561fd51ef2eb36781132449
Sherif Siyanbola <sherifsiyanbola@gmail.com>
2019-11-01 18:49:27 +0100

    added gmail smtp

--- pmp/settings.py
+++ pmp/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-#from decouple import config
-#import dj_database_url
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = '97=ti2+&!62ss9(*zr%5#h+0a7)@vmueug@zcs!2kp^o2+@6p#'
-#SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = '97=ti2+&!62ss9(*zr%5#h+0a7)@vmueug@zcs!2kp^o2+@6p#'
+SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -87,15 +87,15 @@ WSGI_APPLICATION = 'pmp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
+DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
 
 
 # Password validation
@@ -141,7 +141,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 AUTH_USER_MODEL = 'users.User'
@@ -170,5 +170,10 @@ PWA_APP_ICONS = [
 
 
 # email for authentication 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mutarumugyara@gmail.com'
+EMAIL_HOST_PASSWORD = 'keyboard@123'
 
